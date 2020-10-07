89964065f94fde59e87c2bc551fbad2012b24434
Mohamed B <mohamed@jellyfish-commuting.com>
2020-10-07 15:42:10 +0200

    Add timeout of 5 sec

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const blacklist = require('./blacklist');
 // Check disposable providers and DNS MX records
 module.exports = function (email) {
   return new Promise((resolve, reject) => {
+    // Timeout
+    setTimeout(() => reject(new Error('Timeout while checking email address')), 5000);
+
     // Extract domain
     const domain = email.split('@').pop();
 
