fde300307cf70ac6a6819f31bfad605fd2f4cc8a
thejellyfish <the@jellyfish-commuting.com>
2020-11-05 21:58:46 +0100

    Fix named function warning

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dns = require('dns');
 const blacklist = require('./blacklist');
 
 // Check disposable providers and DNS MX records
-module.exports = function revokeIfRiskyEmail(email) {
+module.exports = function (email) {
   return new Promise((resolve, reject) => {
     // Extract domain
     const domain = email.split('@').pop();
