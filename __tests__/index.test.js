17e652d8c85b97af4707427827ee6981ae763114
Mohamed B <mohamed@jellyfish-commuting.com>
2021-03-16 17:21:50 +0100

    Rename package

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,18 @@
-const isRiskyEmail = require('..');
+const isTricky = require('..');
 
 // Start tests ...
 /* eslint-disable no-undef */
 describe('risky-email', () => {
   it('expect error for disposable email', () => Promise.all([
-    isRiskyEmail('sample@yopmail.fr').catch(e => expect(e).not.toBeUndefined()),
-    isRiskyEmail('sample@anonmails.de').catch(e => expect(e).not.toBeUndefined()),
-    isRiskyEmail('sample@jetable.net').catch(e => expect(e).not.toBeUndefined()),
+    isTricky('sample@yopmail.fr').catch(e => expect(e).not.toBeUndefined()),
+    isTricky('sample@anonmails.de').catch(e => expect(e).not.toBeUndefined()),
+    isTricky('sample@jetable.net').catch(e => expect(e).not.toBeUndefined()),
   ]));
 
-  it('expect error for unknow domain', () => isRiskyEmail('sample@non-comp-any.fr').catch(e => expect(e).not.toBeUndefined()));
+  it('expect error for unknow domain', () => isTricky('sample@non-comp-any.fr').catch(e => expect(e).not.toBeUndefined()));
 
   it('expect valid email', () => Promise.all([
-    isRiskyEmail('john.smith@gmail.com').then(email => expect(email).toBe('john.smith@gmail.com')),
-    isRiskyEmail('john.smith@gmx.net').then(email => expect(email).toBe('john.smith@gmx.net')),
+    isTricky('john.smith@gmail.com').then(email => expect(email).toBe('john.smith@gmail.com')),
+    isTricky('john.smith@gmx.net').then(email => expect(email).toBe('john.smith@gmx.net')),
   ]));
 });
